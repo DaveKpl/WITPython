cf9db628807a5ddbedaaec34eae009536dab9443
DaveKpl <92251905+DaveKpl@users.noreply.github.com>
2021-10-10 12:32:02 +0200

    Add files via upload

---
+++ 1-4.py
@@ -0,0 +1,89 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+(100-5**3)/5
+-5.0
+6+(15%4)
+9
+(2**2+24)//4
+7
+type(6)
+<class 'int'>
+type(6.0)
+<class 'float'>
+type(5+3.14)
+<class 'float'>
+int(7.999)
+7
+float(6)
+6.0
+x=2
+x=x+1
+x
+3
+type(x)
+<class 'int'>
+x=x+1.0
+type(x)
+<class 'float'>
+1st_number=1
+SyntaxError: invalid decimal literal
+amount_in_$ = 10
+SyntaxError: invalid syntax
+x=3
+y=5
+x+x-(y*y)
+-19
+x,y=8,5
+print(x)
+8
+print(y)
+5
+bok1, bok2=3,4
+bok3=(bok1**2 + bok2**2)**(1/2)
+bok3
+5.0
+powitanie="Hello"
+powitanie
+'Hello'
+print(powitanie+'user')
+Hellouser
+print(powitanie, 'user')
+Hello user
+print(5*powitanie)
+HelloHelloHelloHelloHello
+driver,brand="Norris", "McLaren"
+print(f"{driver} drives for {brand}")
+Norris drives for McLaren
+print("{} drives for {}".format(driver,brand))
+Norris drives for McLaren
+len(driver)
+6
+len(brand)
+7
+driver.lower()
+'norris'
+driver.upper()
+'NORRIS'
+driver.capitalize()
+'Norris'
+driver.count('r')
+2
+driver[1]
+'o'
+brand[3]
+'a'
+driver[-1]
+'s'
+brand[-3]
+'r'
+driver[1:3]
+'or'
+brand[1:1]
+''
+driver[2:]
+'rris'
+brands="Mrcedes, Red Bull, Mclaren"
+brands.replace("Mrcedes", "Mercedes")
+'Mercedes, Red Bull, Mclaren'
+brands
+'Mrcedes, Red Bull, Mclaren'
